6803bbaf35ad57d4db215b157329dbac800a8d2e
GersoZz <37023982+GersoZz@users.noreply.github.com>
2026-01-10 21:05:32 -0500

    feat: refactor App component to use productsData array

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,29 @@ import "./App.css";
 import ProductCard from "./components/ProductCard";
 
 function App() {
-  const cardId = "card-01";
-  const cardImageUrl =
-    "https://images.unsplash.com/photo-1542291026-7eec264c27ff";
-  const cardTitle = "Zapatillas Runing";
-  const cardText = "Zapatillas ligeras para correr largas distancias.";
-  const cardPrice = 120.5;
+  const productsData = [
+    {
+      id: "card-01",
+      imageUrl: "https://images.unsplash.com/photo-1542291026-7eec264c27ff",
+      title: "Zapatillas Runing",
+      text: "Zapatillas ligeras para correr largas distancias.",
+      price: 120.5,
+    },
+    {
+      id: "card-02",
+      imageUrl: "https://images.unsplash.com/photo-1593369196682-6d8ec9ff3ae0",
+      title: "Cafetera Express",
+      text: "Cafetera de café con leche, leche con café, y café con leche.",
+      price: 199,
+    },
+    {
+      id: "card-03",
+      imageUrl: "https://images.unsplash.com/photo-1579586337278-3befd40fd17a",
+      title: "Reloj Inteligente",
+      text: "Monitor de tiempo inteligente con vista de día y de año.",
+      price: 299,
+    },
+  ];
 
   const handleAddToCart = () => {
     console.log("Agregar al Carrito");
@@ -16,14 +33,19 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <ProductCard
-        id={cardId}
-        imageUrl={cardImageUrl}
-        title={cardTitle}
-        text={cardText}
-        price={cardPrice}
-        handleAddToCart={handleAddToCart}
-      />
+      <div className="card-list">
+        {productsData.map((product) => (
+          <ProductCard
+            key={product.id}
+            id={product.id}
+            imageUrl={product.imageUrl}
+            title={product.title}
+            text={product.text}
+            price={product.price}
+            handleAddToCart={handleAddToCart}
+          />
+        ))}
+      </div>
     </>
   );
 }
