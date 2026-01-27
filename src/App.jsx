89f333e5913e6ae851b45649bcacc5ccc34ad536
GersoZz <37023982+GersoZz@users.noreply.github.com>
2026-01-27 00:33:12 -0500

    feat: add Loader component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import Header from './components/Header'
 import ProductList from './components/ProductList'
 import Container from './components/Container'
 import { productAdapter } from './adapters/products.adapter'
+import Loader from './components/Loader'
 
 function App() {
   const [promoAvailability, setPromoAvailability] = useState(true)
+
   const [productsData, setProductsData] = useState([])
   const [productsSuggestedData, setProductsSuggestedData] = useState([])
   const [productsElectronicsData, setProductsElectronicsData] = useState([])
+
+  const [isLoading, setIsLoading] = useState(true)
 
   const onClose = () => {
     console.log('Desmontar PromoBanner')
@@ -27,6 +31,9 @@ function App() {
       .catch((error) => {
         console.error('Error al obtener los productos:', error)
       })
+      .finally(() => {
+        setIsLoading(false)
+      })
 
     // Para productos sugeridos
     fetch('https://api.escuelajs.co/api/v1/products/?offset=6&limit=3')
@@ -37,6 +44,9 @@ function App() {
       })
       .catch((error) => {
         console.error('Error al obtener los productos:', error)
+      })
+      .finally(() => {
+        setIsLoading(false)
       })
 
     // Para productos electronicos
@@ -49,6 +59,9 @@ function App() {
       .catch((error) => {
         console.error('Error al obtener los productos:', error)
       })
+      .finally(() => {
+        setIsLoading(false)
+      })
   }, [])
 
   return (
@@ -60,21 +73,21 @@ function App() {
         <p style={{ textAlign: 'center', opacity: 0.8, marginBottom: '16px' }}>
           Estos son los productos que puedes comprar.
         </p>
-        <ProductList productsData={productsData} />
+        {isLoading ? <Loader /> : <ProductList productsData={productsData} />}
       </Container>
 
       <Container title="Productos sugeridos">
         <p style={{ textAlign: 'center', opacity: 0.8, marginBottom: '16px' }}>
           Explora nuestra selección de productos recomendados para ti.
         </p>
-        <ProductList productsData={productsSuggestedData} />
+        {isLoading ? <Loader /> : <ProductList productsData={productsSuggestedData} />}
       </Container>
 
       <Container title="Productos electrónicos">
         <p style={{ textAlign: 'center', opacity: 0.8, marginBottom: '16px' }}>
           Descubre nuestra selección de productos electrónicos.
         </p>
-        <ProductList productsData={productsElectronicsData} />
+        {isLoading ? <Loader /> : <ProductList productsData={productsElectronicsData} />}
       </Container>
     </>
   )
