5bbeea448cc2d45d74af96124c63ee47328e9ee5
GersoZz <37023982+GersoZz@users.noreply.github.com>
2026-01-31 22:39:00 -0500

    feat: update Header component to use NavLink for navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import './Header.css'
 import { CartContext } from '../context/CartContext'
+import { Link, NavLink } from 'react-router-dom'
 
 const Header = ({}) => {
   const { cartCount } = useContext(CartContext)
@@ -9,22 +10,22 @@ const Header = ({}) => {
     <header className="header">
       <div className="header-container">
         <div className="header-left">
-          <a href="/" className="header-logo">
+          <Link to="/" className="header-logo">
             ShopStore
-          </a>
+          </Link>
         </div>
         <nav className="header-nav">
-          <a href="/" className="nav-link">
+          <NavLink to="/" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
             Home
-          </a>
-          <a href="/about" className="nav-link">
+          </NavLink>
+          <NavLink to="/about" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
             About
-          </a>
-          <a href="/add-product" className="nav-link">
+          </NavLink>
+          <NavLink to="/add-product" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
             Add Product
-          </a>
+          </NavLink>
         </nav>
-        <a href="/cart" className="header-cart">
+        <Link to="/cart" className="header-cart">
           <svg width="36px" height="36px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
@@ -35,7 +36,7 @@ const Header = ({}) => {
             />
           </svg>
           <span className="cart-count">{cartCount}</span>
-        </a>
+        </Link>
       </div>
     </header>
   )
