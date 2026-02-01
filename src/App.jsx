afbb534c11e328c0fd05bd6b170154900f3a8989
GersoZz <37023982+GersoZz@users.noreply.github.com>
2026-01-31 21:48:50 -0500

    feat: add routing and new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import AddProductPage from './pages/AddProductPage'
+import CartPage from './pages/CartPage'
 
 function App() {
-  return <HomePage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/add-product" element={<AddProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
