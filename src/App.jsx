8af98605bef2838dafaffc9fb304658a9afba675
GersoZz <37023982+GersoZz@users.noreply.github.com>
2026-01-31 22:12:08 -0500

    feat: add Suspense for lazy loading in App component and restructure index.js exports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './Router'
+import { Suspense } from 'react'
+import Loader from './components/Loader'
 
 function App() {
   return (
     <BrowserRouter>
-      <AppRouter />
+      <Suspense fallback={<Loader />}>
+        <AppRouter />
+      </Suspense>
     </BrowserRouter>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,14 @@
+import React from 'react'
 import HomePage from './HomePage'
-import AboutPage from './AboutPage'
-import AddProductPage from './AddProductPage'
-import CartPage from './CartPage'
 
-export { HomePage, AboutPage, AddProductPage, CartPage }
+export { HomePage }
+
+// Promesa
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+export const AboutPage = React.lazy(() => import('./AboutPage'))
+export const AddProductPage = React.lazy(() => import('./AddProductPage'))
+export const CartPage = React.lazy(async () => {
+  await delay(2000)
+  return import('./CartPage')
+})
